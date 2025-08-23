b620e26c8c35be6f38f4dfbd749007318f37df05
Wígny <wignybora@gmail.com>
2025-08-23 13:58:26 -0400

    Simplify fingerprint hash

--- lib/credo_codeclimate.ex
+++ lib/credo_codeclimate.ex
@@ -21,22 +21,49 @@ defmodule CredoCodeClimate do
 
     defp format(issue) do
       %{
+        type: "issue",
+        check_name: Credo.Code.Name.full(issue.check),
         description: issue.message,
-        check_name: String.trim_leading(to_string(issue.check), "Elixir."),
-        fingerprint: Base.encode16(<<:erlang.phash2(issue)::integer-size(32)>>),
-        severity: severity(issue.priority),
+        content: %{body: issue.check.explanations()[:check]},
+        categories: List.wrap(category(issue.check.category())),
         location: %{
           path: issue.filename,
-          lines: %{begin: issue.line_no}
-        }
+          positions: %{
+            begin: %{line: issue.line_no, column: issue.column || 1},
+            end: %{column: column_end(issue.column, issue.trigger)}
+          }
+        },
+        severity: severity(issue.priority),
+        fingerprint: Integer.to_string(:erlang.phash2(issue), 16)
       }
     end
 
-    defp severity(priority) when priority > 19, do: "blocker"
-    defp severity(priority) when priority in 10..19, do: "critical"
-    defp severity(priority) when priority in 0..9, do: "major"
-    defp severity(priority) when priority in -10..-1, do: "minor"
-    defp severity(priority) when priority < -10, do: "info"
+    defp severity(priority) when is_number(priority) do
+      cond do
+        priority > 19 -> :blocker
+        priority in 10..19 -> :critical
+        priority in 0..9 -> :major
+        priority in -10..-1 -> :minor
+        priority < -10 -> :info
+      end
+    end
+
+    defp category(category) do
+      case category do
+        :warning -> "Bug Risk"
+        :readability -> "Clarity"
+        :refactor -> "Complexity"
+        :design -> "Clarity"
+        :consistency -> "Style"
+        :unknown -> nil
+      end
+    end
+
+    defp column_end(column, trigger) do
+      if column && trigger do
+        column + String.length(to_string(trigger))
+      end
+    end
   end
 
   import Credo.Plugin
--- test/credo_codeclimate_test.exs
+++ test/credo_codeclimate_test.exs
@@ -15,23 +15,59 @@ defmodule CredoCodeClimateTest do
              %{
                "check_name" => "Credo.Check.Design.TagTODO",
                "description" => "Found a TODO tag in a comment: # TODO: fix this issue",
-               "fingerprint" => "05656F71",
-               "location" => %{"lines" => %{"begin" => 3}, "path" => "test/fixtures/issues.ex"},
-               "severity" => "major"
+               "fingerprint" => "5656F71",
+               "location" => %{
+                 "path" => "test/fixtures/issues.ex",
+                 "positions" => %{
+                   "begin" => %{"column" => 5, "line" => 3},
+                   "end" => %{"column" => 27}
+                 }
+               },
+               "severity" => "major",
+               "categories" => ["Clarity"],
+               "content" => %{
+                 "body" =>
+                   "TODO comments are used to remind yourself of source code related things.\n\nExample:\n\n    # TODO: move this to a Helper module\n    defp fun do\n      # ...\n    end\n\nThe premise here is that TODO should be dealt with in the near future and\nare therefore reported by Credo.\n\nLike all `Software Design` issues, this is just advice and might not be\napplicable to your project/situation.\n"
+               },
+               "type" => "issue"
              },
              %{
                "check_name" => "Credo.Check.Readability.SinglePipe",
                "description" => "Use a function call when a pipeline is only one function long.",
-               "fingerprint" => "0099DE45",
-               "location" => %{"lines" => %{"begin" => 5}, "path" => "test/fixtures/issues.ex"},
-               "severity" => "critical"
+               "fingerprint" => "99DE45",
+               "location" => %{
+                 "path" => "test/fixtures/issues.ex",
+                 "positions" => %{
+                   "begin" => %{"column" => 5, "line" => 5},
+                   "end" => %{"column" => 7}
+                 }
+               },
+               "severity" => "critical",
+               "categories" => ["Clarity"],
+               "content" => %{
+                 "body" =>
+                   "Pipes (`|>`) should only be used when piping data through multiple calls.\n\nSo while this is fine:\n\n    list\n    |> Enum.take(5)\n    |> Enum.shuffle\n    |> evaluate()\n\nThe code in this example ...\n\n    list\n    |> evaluate()\n\n... should be refactored to look like this:\n\n    evaluate(list)\n\nUsing a single |> to invoke functions makes the code harder to read. Instead,\nwrite a function call when a pipeline is only one function long.\n\nLike all `Readability` issues, this one is not a technical concern.\nBut you can improve the odds of others reading and liking your code by making\nit easier to follow.\n"
+               },
+               "type" => "issue"
              },
              %{
                "check_name" => "Credo.Check.Warning.IoInspect",
                "description" => "There should be no calls to `IO.inspect/1`.",
-               "fingerprint" => "021FEAF4",
-               "location" => %{"lines" => %{"begin" => 5}, "path" => "test/fixtures/issues.ex"},
-               "severity" => "critical"
+               "fingerprint" => "21FEAF4",
+               "location" => %{
+                 "path" => "test/fixtures/issues.ex",
+                 "positions" => %{
+                   "begin" => %{"column" => 8, "line" => 5},
+                   "end" => %{"column" => 18}
+                 }
+               },
+               "severity" => "critical",
+               "categories" => ["Bug Risk"],
+               "content" => %{
+                 "body" =>
+                   "While calls to IO.inspect might appear in some parts of production code,\nmost calls to this function are added during debugging sessions.\n\nThis check warns about those calls, because they might have been committed\nin error.\n"
+               },
+               "type" => "issue"
              }
            ]
   end
