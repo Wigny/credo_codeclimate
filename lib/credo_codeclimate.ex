2f3016b41341121bbd074cd7947c973c9f346b73
Wígny <wignybora@gmail.com>
2025-08-29 14:56:11 -0400

    Support credo diff

--- lib/credo_codeclimate.ex
+++ lib/credo_codeclimate.ex
@@ -12,17 +12,28 @@ defmodule CredoCodeClimate do
       filepath =
         Execution.get_plugin_param(exec, CredoCodeClimate, :report_file) || "codeclimate.json"
 
-      formatted_issues = Enum.map(Execution.get_issues(exec), &format/1)
+      formatted_issues =
+        exec
+        |> Execution.get_issues()
+        |> Enum.filter(&new_issue?/1)
+        |> Enum.map(&format/1)
 
       File.write!(filepath, JSON.encode_to_iodata!(formatted_issues))
 
       exec
     end
 
+    defp new_issue?(%{diff_marker: diff}) when diff in [:new, nil], do: true
+    defp new_issue?(_issue), do: false
+
     defp format(issue) do
+      check_name = Credo.Code.Name.full(issue.check)
+      priority = Credo.Priority.to_atom(issue.priority)
+      hash = :erlang.phash2({check_name, priority, issue.filename, issue.line_no})
+
       %{
         type: "issue",
-        check_name: Credo.Code.Name.full(issue.check),
+        check_name: check_name,
         description: issue.message,
         content: %{body: issue.check.explanations()[:check]},
         categories: List.wrap(category(issue.check.category())),
@@ -33,8 +44,8 @@ defmodule CredoCodeClimate do
             end: %{column: column_end(issue.column, issue.trigger)}
           }
         },
-        severity: severity(Credo.Priority.to_atom(issue.priority)),
-        fingerprint: Integer.to_string(:erlang.phash2(issue), 16)
+        severity: severity(priority),
+        fingerprint: Integer.to_string(hash, 16)
       }
     end
 
@@ -44,16 +55,12 @@ defmodule CredoCodeClimate do
     defp severity(:low), do: :minor
     defp severity(:ignore), do: :info
 
-    defp category(category) do
-      case category do
-        :warning -> "Bug Risk"
-        :readability -> "Clarity"
-        :refactor -> "Complexity"
-        :design -> "Clarity"
-        :consistency -> "Style"
-        :unknown -> nil
-      end
-    end
+    defp category(:warning), do: "Bug Risk"
+    defp category(:readability), do: "Clarity"
+    defp category(:refactor), do: "Complexity"
+    defp category(:design), do: "Clarity"
+    defp category(:consistency), do: "Style"
+    defp category(:unknown), do: nil
 
     defp column_end(column, trigger) do
       if column && trigger do
@@ -63,15 +70,13 @@ defmodule CredoCodeClimate do
   end
 
   import Credo.Plugin
+  alias Credo.CLI.Command
 
   @doc false
   def init(exec) do
     exec
     |> register_cli_switch(:report_file, :string)
-    |> append_task(
-      Credo.CLI.Command.Suggest.SuggestCommand,
-      :print_after_analysis,
-      GenerateReport
-    )
+    |> append_task(Command.Suggest.SuggestCommand, :print_after_analysis, GenerateReport)
+    |> append_task(Command.Diff.DiffCommand, :print_after_analysis, GenerateReport)
   end
 end
--- test/credo_codeclimate_test.exs
+++ test/credo_codeclimate_test.exs
@@ -1,74 +1,91 @@
 defmodule CredoCodeClimateTest do
   use ExUnit.Case, async: true
 
+  @report [
+    %{
+      "check_name" => "Credo.Check.Design.TagTODO",
+      "description" => "Found a TODO tag in a comment: # TODO: fix this issue",
+      "fingerprint" => "3A56884",
+      "location" => %{
+        "path" => "test/fixtures/issues.ex",
+        "positions" => %{
+          "begin" => %{"column" => 5, "line" => 3},
+          "end" => %{"column" => 27}
+        }
+      },
+      "severity" => "major",
+      "categories" => ["Clarity"],
+      "content" => %{
+        "body" =>
+          "TODO comments are used to remind yourself of source code related things.\n\nExample:\n\n    # TODO: move this to a Helper module\n    defp fun do\n      # ...\n    end\n\nThe premise here is that TODO should be dealt with in the near future and\nare therefore reported by Credo.\n\nLike all `Software Design` issues, this is just advice and might not be\napplicable to your project/situation.\n"
+      },
+      "type" => "issue"
+    },
+    %{
+      "check_name" => "Credo.Check.Readability.SinglePipe",
+      "description" => "Use a function call when a pipeline is only one function long.",
+      "fingerprint" => "7F38538",
+      "location" => %{
+        "path" => "test/fixtures/issues.ex",
+        "positions" => %{
+          "begin" => %{"column" => 5, "line" => 5},
+          "end" => %{"column" => 7}
+        }
+      },
+      "severity" => "critical",
+      "categories" => ["Clarity"],
+      "content" => %{
+        "body" =>
+          "Pipes (`|>`) should only be used when piping data through multiple calls.\n\nSo while this is fine:\n\n    list\n    |> Enum.take(5)\n    |> Enum.shuffle\n    |> evaluate()\n\nThe code in this example ...\n\n    list\n    |> evaluate()\n\n... should be refactored to look like this:\n\n    evaluate(list)\n\nUsing a single |> to invoke functions makes the code harder to read. Instead,\nwrite a function call when a pipeline is only one function long.\n\nLike all `Readability` issues, this one is not a technical concern.\nBut you can improve the odds of others reading and liking your code by making\nit easier to follow.\n"
+      },
+      "type" => "issue"
+    },
+    %{
+      "check_name" => "Credo.Check.Warning.IoInspect",
+      "description" => "There should be no calls to `IO.inspect/1`.",
+      "fingerprint" => "3A177D0",
+      "location" => %{
+        "path" => "test/fixtures/issues.ex",
+        "positions" => %{
+          "begin" => %{"column" => 8, "line" => 5},
+          "end" => %{"column" => 18}
+        }
+      },
+      "severity" => "critical",
+      "categories" => ["Bug Risk"],
+      "content" => %{
+        "body" =>
+          "While calls to IO.inspect might appear in some parts of production code,\nmost calls to this function are added during debugging sessions.\n\nThis check warns about those calls, because they might have been committed\nin error.\n"
+      },
+      "type" => "issue"
+    }
+  ]
+
   @tag :tmp_dir
-  test "creates a report file", %{tmp_dir: tmp_dir} do
-    Credo.run(~w[
-      test/fixtures/issues.ex
-        --config-file test/fixtures/.credo.exs
-        --report-file #{tmp_dir}/report.json
+  test "suggest creates a report file", %{tmp_dir: tmp_dir} do
+    Credo.run(~w[suggest test/fixtures/issues.ex
+      --config-file test/fixtures/.credo.exs
+      --report-file #{tmp_dir}/report.json
     ])
 
-    assert file = File.read!(Path.join(tmp_dir, "report.json"))
+    assert read_report(tmp_dir) == @report
+  end
 
-    assert JSON.decode!(file) == [
-             %{
-               "check_name" => "Credo.Check.Design.TagTODO",
-               "description" => "Found a TODO tag in a comment: # TODO: fix this issue",
-               "fingerprint" => "5656F71",
-               "location" => %{
-                 "path" => "test/fixtures/issues.ex",
-                 "positions" => %{
-                   "begin" => %{"column" => 5, "line" => 3},
-                   "end" => %{"column" => 27}
-                 }
-               },
-               "severity" => "major",
-               "categories" => ["Clarity"],
-               "content" => %{
-                 "body" =>
-                   "TODO comments are used to remind yourself of source code related things.\n\nExample:\n\n    # TODO: move this to a Helper module\n    defp fun do\n      # ...\n    end\n\nThe premise here is that TODO should be dealt with in the near future and\nare therefore reported by Credo.\n\nLike all `Software Design` issues, this is just advice and might not be\napplicable to your project/situation.\n"
-               },
-               "type" => "issue"
-             },
-             %{
-               "check_name" => "Credo.Check.Readability.SinglePipe",
-               "description" => "Use a function call when a pipeline is only one function long.",
-               "fingerprint" => "99DE45",
-               "location" => %{
-                 "path" => "test/fixtures/issues.ex",
-                 "positions" => %{
-                   "begin" => %{"column" => 5, "line" => 5},
-                   "end" => %{"column" => 7}
-                 }
-               },
-               "severity" => "critical",
-               "categories" => ["Clarity"],
-               "content" => %{
-                 "body" =>
-                   "Pipes (`|>`) should only be used when piping data through multiple calls.\n\nSo while this is fine:\n\n    list\n    |> Enum.take(5)\n    |> Enum.shuffle\n    |> evaluate()\n\nThe code in this example ...\n\n    list\n    |> evaluate()\n\n... should be refactored to look like this:\n\n    evaluate(list)\n\nUsing a single |> to invoke functions makes the code harder to read. Instead,\nwrite a function call when a pipeline is only one function long.\n\nLike all `Readability` issues, this one is not a technical concern.\nBut you can improve the odds of others reading and liking your code by making\nit easier to follow.\n"
-               },
-               "type" => "issue"
-             },
-             %{
-               "check_name" => "Credo.Check.Warning.IoInspect",
-               "description" => "There should be no calls to `IO.inspect/1`.",
-               "fingerprint" => "21FEAF4",
-               "location" => %{
-                 "path" => "test/fixtures/issues.ex",
-                 "positions" => %{
-                   "begin" => %{"column" => 8, "line" => 5},
-                   "end" => %{"column" => 18}
-                 }
-               },
-               "severity" => "critical",
-               "categories" => ["Bug Risk"],
-               "content" => %{
-                 "body" =>
-                   "While calls to IO.inspect might appear in some parts of production code,\nmost calls to this function are added during debugging sessions.\n\nThis check warns about those calls, because they might have been committed\nin error.\n"
-               },
-               "type" => "issue"
-             }
-           ]
+  @tag :tmp_dir
+  test "diff creates a report file", %{tmp_dir: tmp_dir} do
+    Credo.run(~w[diff
+      --from-dir test/fixtures
+      --config-file test/fixtures/.credo.exs
+      --report-file #{tmp_dir}/report.json
+    ])
+
+    assert read_report(tmp_dir) == @report
+  end
+
+  defp read_report(tmp_dir) do
+    tmp_dir
+    |> Path.join("report.json")
+    |> File.read!()
+    |> JSON.decode!()
   end
 end
